306100df7b665f209f7ab7f9db42a9d9e0385f1d
Truong Hai Dang <haidanghth910@gmail.com>
2020-10-31 20:17:47 +0700

    Dataset

--- Form1.cs
+++ Form1.cs
@@ -14,9 +14,6 @@ namespace Sudoku_Solver
     public partial class main : Form
     {
         private int[,] sudoku_matrix = new int[9, 9];
-        private bool[,] sudoku_col_matrix = new bool[9, 9];
-        private bool[,] sudoku_row_matrix = new bool[9, 9]; 
-        private bool[,] sudoku_area_matrix = new bool[9, 9];
         private int[] arr_tmp = new int[81];
         public main()
         {
@@ -31,7 +28,7 @@ namespace Sudoku_Solver
         private void solve_matrix_Click(object sender, EventArgs e)
         {
             init_sudoku_matrix();
-            //solve_sudoku_maxtrix();
+            solve_sudoku_maxtrix(sudoku_matrix, sudoku_matrix.GetLength(0) -1, sudoku_matrix.GetLength(1)-1);
         }
 
         public void init_sudoku_matrix()
@@ -61,41 +58,56 @@ namespace Sudoku_Solver
             }    
         }
 
-        public void check_valid()
+        public bool check_valid(int[,] matrix_array, int row, int col, int value)
         {
-            for(int i = 0; i < sudoku_matrix.GetLength(0); i++)
+            for(int i = 0; i < 9; i++)
             {
-                for (int j = 0; j < sudoku_matrix.GetLength(1); j++)
+                if (matrix_array[row, i] == value) return false;
+            }
+            for (int j = 0; j < 9; j++)
+            {
+                if (matrix_array[j, col] == value) return false;
+            }
+            int a = row / 3, b = col / 3;
+            for (int i = 3 * a; i < 3 * a + 3; i++)
+            {
+                for (int j = 3 * b; j < 3 * b + 3; j++)
                 {
-                    if(sudoku_matrix[i, j] != 0)
-                    {
-                        sudoku_row_matrix[i, j] = true;
-                        sudoku_col_matrix[j, j] = true;
-                    }    
+                    if (matrix_array[i, j] == value) return false;
                 }
-            }    
+            }
+            return true;
         }
 
-        public void solve_sudoku_maxtrix(int i, int j)
+        public void solve_sudoku_maxtrix(int[,] matrix_array, int row, int col)
         {
-            if(i < 9 && j < 9)
+            if (col == 9)
             {
-                if(sudoku_matrix[i, j] == 0)
+                if (row == 8)
                 {
-                    for(int k = 1; k <= 9; k++)
-                    {
-
-                    }    
-                }    
-            }  
-            else if(i < 9 && j >= 9)
+                    MessageBox.Show(matrix_array.ToString());
+                }
+                else
+                {
+                    solve_sudoku_maxtrix(matrix_array, row + 1, 0);
+                }
+            }
+            else if (matrix_array[row, col] == 0)
             {
-
-            }    
+                for (int k = 1; k <= 9; k++)
+                {
+                    if (check_valid(matrix_array, row, col, k))
+                    {
+                        matrix_array[row, col] = k;
+                        solve_sudoku_maxtrix(matrix_array, row, col+1);
+                        matrix_array[row, col] = 0;
+                    }
+                }
+            }
             else
             {
-
-            }    
+                solve_sudoku_maxtrix(matrix_array, row, col + 1);
+            }
         }
 
         private void Check_result_Click(object sender, EventArgs e)
@@ -105,11 +117,36 @@ namespace Sudoku_Solver
 
         private void new_matrix_Click(object sender, EventArgs e)
         {
-            int i = 0;
-            foreach (Control element in input.Controls.OfType<TextBox>())
-            {
-                element.Text = (i++).ToString();
-            }
+            textBox81.Text = "5";
+            textBox54.Text = "3";
+            textBox53.Text = "7";
+            textBox78.Text = "6";
+            textBox77.Text = "1";
+            textBox50.Text = "9";
+            textBox17.Text = "5";
+            textBox48.Text = "9";
+            textBox74.Text = "8";
+            textBox24.Text = "6";
+            textBox45.Text = "8";
+            textBox15.Text = "6";
+            textBox23.Text = "3";
+            textBox42.Text = "4";
+            textBox41.Text = "8";
+            textBox14.Text = "3";
+            textBox22.Text = "1";
+            textBox39.Text = "7";
+            textBox64.Text = "2";
+            textBox21.Text = "6";
+            textBox62.Text = "6";
+            textBox34.Text = "2";
+            textBox60.Text = "8";
+            textBox32.Text = "4";
+            textBox58.Text = "1";
+            textBox11.Text = "9";
+            textBox19.Text = "5"; 
+            textBox55.Text = "8";
+            textBox2.Text = "7";
+            textBox1.Text = "9";
         }
     }
 }
